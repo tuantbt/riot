f6ef1913ce7efcc39a86ad4425e5d5ac560cd62e
tuantbt <hotuan1155@gmail.com>
2017-05-11 06:36:20 +0700

    Add files via upload

--- cron.php
+++ cron.php
@@ -1,38 +1,40 @@
 <?php
 
-include 'user.php';
 
-login("http://vipfb.us/verify.php?user=".$token1."","null");
-echo post_data("http://vipfb.us/f.php?type=custom","id=".$UID."&limit=100&submit=Submit");
+/** 
+Script CUrl By HocTrick.NET
+ **/
 
-login("http://vipfb.us/verify.php?user=".$token2."","null");
-echo post_data("http://vipfb.us/f.php?type=custom","id=".$UID."&limit=100&submit=Submit");
+include'config.php';
 
-login("http://vipfb.us/verify.php?user=".$token3."","null");
-echo post_data("http://vipfb.us/f.php?type=custom","id=".$UID."&limit=100&submit=Submit");
+$token = file_get_contents('token/token.txt');
+$token2 = file_get_contents('token/token2.txt');
+$token3 = file_get_contents('token/token3.txt');
+$token4 = file_get_contents('token/token4.txt');
+$token5 = file_get_contents('token/token5.txt');
 
-login("http://vipfb.us/verify.php?user=".$token4."","null");
-echo post_data("http://vipfb.us/f.php?type=custom","id=".$UID."&limit=100&submit=Submit");
 
-login("http://vipfb.us/verify.php?user=".$token1."","null");
-echo post_data("http://vipfb.us/f.php?type=custom","id=100014148976494&limit=100&submit=Submit");
+kunkey($dm.'/viplike.php?id='.$uid.'&token='.$token);
+echo '<hr>';
+kunkey($dm.'/viplike.php?id='.$uid.'&token='.$token2);
+echo '<hr>';
+kunkey($dm.'/viplike.php?id='.$uid.'&token='.$token3);
+echo '<hr>';
+kunkey($dm.'/viplike.php?id='.$uid.'&token='.$token4);
+echo '<hr>';
+kunkey($dm.'/viplike.php?id='.$uid.'&token='.$token5);
 
-function post_data($site,$data){
-    $datapost = curl_init();
-    $headers = array("Expect:");
-    curl_setopt($datapost, CURLOPT_URL, $site);
-    curl_setopt($datapost, CURLOPT_TIMEOUT, 40000);
-    curl_setopt($datapost, CURLOPT_HEADER, TRUE);
-    curl_setopt($datapost, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($datapost, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-    curl_setopt($datapost, CURLOPT_POST, TRUE);
-    curl_setopt($datapost, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($datapost, CURLOPT_COOKIEFILE, "cookie.txt");
-    ob_start();
-     curl_exec ($datapost);
-    ob_end_clean();
-    curl_close ($datapost);
-    unset($datapost); 
-}
 
-?>
+unlink("logvip.txt"); 
+
+
+
+function kunkey($url){
+   $curl = curl_init();
+   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($curl, CURLOPT_URL, $url);
+   $ch = curl_exec($curl);
+   curl_close($curl);
+   echo $ch;
+   }
+?>	
